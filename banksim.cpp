f321a974f7ba68a0cccd74cb1ab737f9257da51b
Emelie Johansen <emelie_johansen@hotmail.com>
2021-12-05 16:36:06 +0100

    Lagt till funktioner för insättning och uttag.

--- banksim.cpp
+++ banksim.cpp
@@ -18,23 +18,50 @@ utifrån angiven ränta, aktuellt saldo och storlek på månadsinsättningen.
 #include <iostream>
 using namespace std;
 
-int menyVal;
+char menyVal;
+int insBelopp = 0, saldo = 0, uttagsBelopp = 0;
 
 int main () {
 
-     // Rensa skärmen
-    cout << "\033[2J\033[1;1H";
-    cout << "\n";
+    // While loop för att hantera huvudmenyn
+    while (menyVal != 'A') {
+        // Rensa skärmen
+        cout << "\033[2J\033[1;1H";
+        cout << "\n";
 
-    // Skriv ut huvudmeny
-    cout << "[I]nsättning\n\n";
-    cout << "[U]ttag\n\n";
-    cout << "[S]aldo\n\n";
-    cout << "[R]äntebetalning\n\n";
-    cout << "[A]vsluta\n\n";
+        // Skriv ut huvudmeny
+        cout << "[I]nsättning\n\n";
+        cout << "[U]ttag\n\n";
+        cout << "[S]aldo\n\n";
+        cout << "[R]äntebetalning\n\n";
+        cout << "[A]vsluta\n\n";
 
-    cout << "\n\nAnge menyval: ";
-    cin >> menyVal;
+        cout << "\n\nAnge menyval: ";
+        cin >> menyVal;
+
+        switch (menyVal) {
+            case 'I':
+                cout << "\n\nAnge belopp att sätta in: ";
+                cin >> insBelopp; 
+                saldo = saldo + insBelopp;
+                cout << "\nDitt saldo är nu: ";
+                cout << saldo;
+                cout << "\n\nTryck 0 för att komma vidare\n";
+                cin >> insBelopp;
+                break;
+            case 'U':
+                cout << "\n\nAnge belopp att ta ut: ";
+                cin >> uttagsBelopp; 
+                saldo = saldo - uttagsBelopp;
+                cout << "\nDitt saldo är nu: ";
+                cout << saldo;
+                cout << "\n\nTryck 0 för att komma vidare\n";
+                cin >> insBelopp;
+                break;
+            
+
+        }
+    }
 
     return 0;
 }
