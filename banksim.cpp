910a4e0816768688a19bc2619913f7af3ee7d9be
Emelie Johansen <emelie_johansen@hotmail.com>
2021-12-06 19:43:03 +0100

    Lagt till kommentarer och utvärdering

--- banksim.cpp
+++ banksim.cpp
@@ -1,5 +1,5 @@
 /* 
-Applikation: banksimulator
+Applikation: Banksimulator
 Skapad av: Emelie Johansen
 Skapad: 2021-12-05
 Version: 1.0.0
@@ -18,31 +18,62 @@ utifrån angiven ränta, aktuellt saldo och storlek på månadsinsättningen.
 #include <iostream>
 using namespace std;
 
+// Skapa variabel för menyvalet. Typen char räcker eftersom den bara ska lagra ett tecken
+
 char menyVal;
+
+// Skapa variabler för insatt belopp, uttaget belopp, månadsinsättning och räntesats
+// Typen double är vald för att kunna hantera stora tal med decimaler
+
 double insBelopp = 0, uttagsBelopp = 0;
 double mIns, rSats;
+
+// Skapa variabel som håller antalet år vi måste spara för att uppnå ett målsaldo
+// Typen int är vald eftersom variabeln ska innehålla antalet hela år
+
 int antalAr = 0;
+
+// Skapa variabler för saldo och målsaldo. Skapa även en variabel för att hålla ett tillfälligt saldo
+// som kan användas i beräkningarna.
+// Typen double är vald för att kunna hantera stora tal med decimaler 
+
 double tempSaldo = 0, saldo = 0, malSaldo;
+
+// Funktionen main() returnerar ett heltalsvärde (int)
 
 int main () {
 
     // While loop för att hantera huvudmenyn
+    // Så länge användaren inte väljer att avsluta kommer huvudmenyn att skrivas ut på nytt
+
     while (menyVal != 'A') {
+
         // Rensa skärmen
+
         cout << "\033[2J\033[1;1H";
         cout << "\n";
 
         // Skriv ut huvudmeny
+
         cout << "[I]nsättning\n\n";
         cout << "[U]ttag\n\n";
         cout << "[S]aldo\n\n";
         cout << "[R]äntebetalning\n\n";
         cout << "[A]vsluta\n\n";
 
+        // Läs in användarens menyval och lagra svaret i en variabel
+
         cout << "\n\nAnge menyval: ";
         cin >> menyVal;
 
+        // En switch för att hantera alla de olika menyvalen
+
         switch (menyVal) {
+
+            // Om användaren väljer insättning skriver vi ut aktuellt saldo och 
+            // ber användaren att ange beloppet som ska sättas in. Det nya saldot
+            // skrivs ut på skärmen för att bekräfta insättningen.
+
             case 'I': case 'i':
                 cout << "\n\nAktuellt saldo är: ";
                 cout << saldo;
@@ -54,6 +85,11 @@ int main () {
                 cout << "\n\nTryck 0 för att komma vidare\n";
                 cin >> insBelopp;
                 break;
+
+            // Om användaren väljer uttag skriver vi ut aktuellt saldo och 
+            // ber användaren att ange beloppet som ska tas ut. Det nya saldot
+            // skrivs ut på skärmen för att bekräfta uttaget.
+
             case 'U': case 'u':
                 cout << "\n\nAktuellt saldo är: ";
                 cout << saldo;
@@ -65,12 +101,20 @@ int main () {
                 cout << "\n\nTryck 0 för att komma vidare\n";
                 cin >> insBelopp;
                 break;
+
+            // Om användaren väljer saldo skriver vi ut det aktuella saldot. 
+
             case 'S': case 's':
                 cout << "\nDitt saldo är: ";
                 cout << saldo;
                 cout << "\n\nTryck 0 för att komma vidare\n";
                 cin >> insBelopp;
                 break;   
+
+            // Om användaren väljer räntebetalning frågar vi efter ett antal 
+            // olika parametrar och räknar därefter ut hur lång tid det tar
+            // att nå önskat målsaldo.
+
             case 'R': case 'r':
                 cout << "\nHur mycket pengar är ditt mål att ha på kontot?: ";
                 cin >> malSaldo;
@@ -81,6 +125,10 @@ int main () {
                 cout << "\nDitt nuvarande saldo är: ";
                 cout << saldo;
                 tempSaldo = saldo;
+
+                // Här används formeln för att räkna ut hur många år det tar
+                // att uppnå önskat målsaldo.
+
                 while (tempSaldo < malSaldo) {
                         tempSaldo = (tempSaldo * (1 + rSats / 100) + (mIns * 12));
                         antalAr++;
@@ -90,9 +138,17 @@ int main () {
                 cout << "\n\nTryck 0 för att komma vidare\n";
                 cin >> insBelopp;
                 break;
+
+            // Om användaren väljer att avsluta tackar vi för användningen och
+            // avslutar hela programmet.
+
             case 'A': case 'a':
                 cout << "\n\nTack för att du använde banksimulatorn!\n\n";
                 return 0;   
+
+            // Om användaren anger ett ogiltigt val skriver vi ut ett felmeddelande och
+            // användaren skickas tillbaka till huvudmenyn. 
+
             default:
                 cout << "\n\nOgiltigt menyval! Försök igen";
                 cout << "\n\nTryck 0 för att komma vidare\n";
@@ -104,5 +160,36 @@ int main () {
         }
     }
 
+    // Returnera alltid siffran 0
+    // Framtida versioner kan tänkas returnera andra siffror 
+    // beroende på vad som händer i programmet, t.ex. felkoder
+
     return 0;
 }
+
+/* 
+Utvärdering
+
+Banksimulatorn var intressant att utforma eftersom den innehåller så många olika delar.
+Huvudmenyn med sina olika alternativ valde jag att göra med hjälp av en switch som
+hanterar de olika funktionerna i simulatorn. Det gör att jag har fått en förståelse för
+hur jag enkelt kan komplettera med ytterligare funktioner i framtida versioner av simulatorn.
+
+Jag bedömer att användarvänligheten är hög eftersom alla menyval är självförklarande och att 
+simulatorn hela tiden skriver ut information på skärmen åt användaren. 
+
+Banksimulatorn innehåller flera verklighetstrogna funktioner som används i riktiga banker.
+Funktioner som insättning, uttag och saldobesked finns på alla banker.
+Att räkna ut antal år det tar att spara ihop till ett visst belopp var väldigt intressant för
+mig eftersom jag då enkelt kan räkna på olika scenarion. 
+
+På en riktig bank sparas saldot för varje kund även om programmet avslutas men i den här
+simulatorn nollställs saldot vid varje avslut och den kan bara hantera en kund.
+Framtida versioner av simulatorn skulle kunna be användaren att logga in och spara alla 
+transaktioner i en fil som lagras på hårddisken.
+
+Framtida versioner bör också kompletteras med robusta felkontroller som kontrollerar alla
+inmatningar användaren gör innan variablarna används i simulatorn. Oväntade fel behöver
+också fångas upp och hanteras på ett snyggt sätt så att informativa felmeddelanden skrivs
+ut på skärmen.
+*/
